f958ca4e17804ab5374ae52bf1037f0d4a558c09
David Bertholt <dbertholt@veritran.com>
2023-07-13 01:40:42 -0300

    comment style

--- src/view/components/Path/Path.tsx
+++ src/view/components/Path/Path.tsx
@@ -24,7 +24,7 @@ export default Path;
 
 const PathContainer = styled.div`
   min-height: 60px;
-  min-width: 150vh;
+  /* min-width: 150vh; */
   margin-top: 1rem;
   display: flex;
   text-align: end;
